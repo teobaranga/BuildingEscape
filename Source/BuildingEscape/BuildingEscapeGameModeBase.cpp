30444664a263847f12626f8b4cea08480fb922be
Teo Baranga <teodor08@live.com>
2017-12-10 19:23:03 -0500

    Initial commit

---
+++ Source/BuildingEscape/BuildingEscapeGameModeBase.cpp
@@ -0,0 +1,9 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "BuildingEscapeGameModeBase.h"
+
+
+void ABuildingEscapeGameModeBase::StartPlay() {
+	UE_LOG(LogTemp, Log, TEXT("Hello"));
+}
+
